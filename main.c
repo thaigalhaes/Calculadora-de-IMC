cf4f577508020b00af9023eb85877fe3fef6d3ee
Thainá Magalhães de Sousa <80012076+thaigalhaes@users.noreply.github.com>
2022-07-21 15:13:57 -0300

    main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(v) {
+  int Peso =0;
+  float IMC =0, Altura =0;
+  printf("Peso: ");
+  scanf("%d" ,&Peso);
+  printf("Altura: ");
+  scanf("%f", &Altura);
+
+  IMC = Peso/ (Altura*Altura);
+  printf("Seu IMC é: %.2f", IMC);
+  return 0;
+}
